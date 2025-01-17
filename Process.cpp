9da82be89a5671e330b402184870a4954f4ee648
MojtabaKD <mkbox86@gmail.com>
2025-01-17 15:23:12 +0330

    Some few proc info

--- Process.cpp
+++ Process.cpp
@@ -1,20 +1,26 @@
-// Proces.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <psapi.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void ShowProcessInfo() {
+    DWORD processID = GetCurrentProcessId();
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+
+    if (hProcess != nullptr) {
+        PROCESS_MEMORY_COUNTERS pmc;
+        if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
+            std::cout << "Process ID: " << processID << std::endl;
+            std::cout << "Working Set Size: " << pmc.WorkingSetSize << " bytes" << std::endl;
+            std::cout << "Pagefile Usage: " << pmc.PagefileUsage << " bytes" << std::endl;
+        }
+        CloseHandle(hProcess);
+    }
+    else {
+        std::cerr << "Failed to open process!" << std::endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+    ShowProcessInfo();
+    return 0;
+}
